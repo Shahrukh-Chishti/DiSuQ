3ab6d4c88957319b225b5521e7fba90238c3f469
shahrukh <shahrukh.chishti@gmail.com>
2021-04-20 15:12:48 +0530

    generic function in AnaGS

--- AnaGS/test.py
+++ AnaGS/test.py
@@ -5,30 +5,32 @@ import matplotlib.pyplot as plt
 
 quintic = [1,40,0,-2,12,10]
 cubic = [2,1,4,3]
+generic = [2,23,45,23,1,41,36,16,13,61,61,3,8,6,9,6]
 x0 = [-1]
 x0 = numpy.array(x0)
 
-roots = numpy.roots(cubic)
+roots = numpy.roots(generic)
 print('roots',roots)
 print('smallest root',min(roots))
 
-f = polynomial(cubic)
+f = polynomial(generic)
 loss = lambda x:f(x)*f(x)
 
 print('f(root)',f(min(roots)))
-print('polyval(roots)',numpy.polyval(cubic,min(roots)))
+print('polyval(roots)',numpy.polyval(generic,min(roots)))
 #x = solve_smallest_root(cubic,x0,50)
 #print('optimised solution:',x)
 
-I = gradient(cubic)
+I = gradient(generic)
 res = sp_minimize(loss,x0,method='Newton-CG',jac=I)
 print(res)
 
 iteration_set = numpy.arange(3,10,dtype=int)
-starting_set = numpy.arange(-10,0,.5)
-ground_eval = scipy_ground_state(cubic,iteration_set,starting_set)
+starting_set = numpy.arange(-15,-5,.5)
+ground_eval = scipy_ground_state(generic,iteration_set,starting_set)
 error = abs(ground_eval-min(roots))
 
+## plot dynamics for iterative algorithm
 fig,ax = plt.subplots(nrows=1,ncols=2)
 
 bp = ax[0].boxplot(error)
