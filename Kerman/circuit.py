43296b3859a709233944188ecec2137d6eccc1e1
shahrukh <shahrukh.chishti@gmail.com>
2021-05-24 21:08:12 +0530

    debug components superclass

--- Kerman/circuit.py
+++ Kerman/circuit.py
@@ -6,6 +6,12 @@ from jax import grad, jit
 from jax import random
 
 flux_quanta = 1
+h = 1
+
+def modeTensorProduct():
+    """
+        extend mode to full system basis
+    """
 
 def modeQuantProduct(A,M,B):
     """
@@ -25,13 +31,15 @@ class Circuit:
     def __init__(self,network):
         self.network = network
         self.G = self.parseCircuit()
-        self.Nn = self.G.number_of_nodes()
-        self.Nb = self.G.number_of_edges()
+        self.nodes = self.nodeIndex()
+        self.edges = self.edgesIndex()
+        self.Nn = len(self.nodes)
+        self.Nb = len(self.edges)
 
     def parseCircuit(self):
         G = networkx.Graph()
         for component in self.network:
-            G.add_edge(component.minus,component.plus,component)
+            G.add_edge(component.minus,component.plus,component=component)
         return G
 
     def transformComponents(self):
@@ -57,9 +65,14 @@ class Circuit:
 
         return Lo_,C_
 
-    def hamiltonian(self):
+    def hamiltonian(self,basis):
+        """
+            basis : {o:(,,,),i:(,,,),j:(,,,)}
+        """
         Lo_,C_ = self.transformComponents()
         Co_,Coi_,Coj_,Ci_,Cij_,Cj_ = C_
+
+        Zi = numpy.sqrt(numpy.diagonal(Co_)/numpy.diagonal(Lo_))
 
     def modeDistribution(self):
         return No,Ni,Nj
@@ -71,35 +84,38 @@ class Circuit:
     def graphGL(self):
         GL = self.G.copy()
         for u,v,component in GL.edges(data=True):
+            component = component['component']
             if component.__class__ == C:
                 GL.remove_edge(u,v)
         return GL
 
     def nodeIndex(self):
-        nodes = dict(zip(enumerate(self.G.nodes())))
+        nodes = dict([*enumerate([*self.G.nodes()])])
         return nodes
 
     def edgesIndex(self):
-        edges = dict(zip(enumerate(self.G.edges())))
+        edges = dict([*enumerate([*self.G.edges()])])
         return edges
 
     def nodeCapacitance(self):
         Cn = numpy.zeros((self.Nn,self.Nn))
-        nodes = self.nodeIndex()
-        for i,node in enumerate(self.G.nodes()):
+        for i,node in self.nodes.items():
             node_capacitance = 0
-            for u,v,component in self.G.edges(node,date=True):
+            for u,v,component in self.G.edges(node,data=True):
+                component = component['component']
                 if component.__class__ == C:
                     node_capacitance += component.capacitance
             Cn[i,i] = node_capacitance
+            # off diagonals path non-uniqueness
 
         Cn_ = inverse(Cn)
         return Cn_
 
     def branchInductance(self):
         Lb = numpy.zeros((self.Nb,self.Nb))
-        for i,(u,v) in self.edges():
+        for i,(u,v) in self.edges.items():
             component = self.G[u][v]
+            component = component['component']
             if component.__class__ == L:
                 Lb[i,i] = component.inductance
         return Lb
@@ -110,14 +126,16 @@ class Circuit:
 
     def connectionPolarity(self):
         Rbn = numpy.zeros((self.Nb,self.Nn),int)
+        node_ = {val:key for key,val in self.nodes.items()}
+        for i,(u,v) in self.edges:
+            Rbn[i][node_[u]] = 1
+            Rbn[i][node_[v]] = -1
         return Rbn
 
     def modeTransformation(self,Ln_):
         return R
 
-
-
 if __name__=='__main__':
     transmon = [J(0,1,10),C(0,1,5)]
     transmon = Circuit(transmon)
-
+    import ipdb;ipdb.set_trace()
--- Kerman/components.py
+++ Kerman/components.py
@@ -1,3 +1,7 @@
+import numpy
+
+def basisQo(modes):
+    return basis
 
 class Elements:
     def __init__(self,plus,minus):
@@ -6,15 +10,15 @@ class Elements:
 
 class J(Elements):
     def __init__(self,plus,minus,energy):
-        super(J, self).__init__(plus,minus)
+        super().__init__(plus,minus)
         self.energy = energy
 
 class C(Elements):
     def __init__(self,plus,minus,capacitance):
-        super(J, self).__init__(plus,minus)
+        super().__init__(plus,minus)
         self.capacitance = capacitance
 
 class L(Elements):
     def __init__(self,plus,minus,inductance):
-        super(J, self).__init__(plus,minus)
+        super().__init__(plus,minus)
         self.inductance = inductance
