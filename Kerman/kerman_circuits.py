9fffe381343f98c10278bc9fe02f71455b4ea424
shahrukh <shahrukh.chishti@gmail.com>
2021-12-29 18:53:02 +0100

    phase slip - variable parameters

--- Kerman/kerman_circuits.py
+++ Kerman/kerman_circuits.py
@@ -31,17 +31,19 @@ def shuntedQubit(basis):
     circuit = Circuit(circuit,basis)
     return circuit
 
-def phaseSlip(basis):
-    circuit = [C(0,1,100)]
-    circuit += [L(1,3,.001,'Ltl',True),L(1,4,.0005,'Lbl',True)]
+def phaseSlip(basis,inductance=[.001,.0005,.00002,.00035,.0005],capacitance=[100,30,30,30,30,40,10]):
+    La,Lb,Lc,Ld,Le = inductance
+    Ca,Cb,Cc,Cd,Ce,Cf,Cg = capacitance
+    circuit = [C(0,1,Ca)]
+    circuit += [L(1,3,La,'Ltl',True),L(1,4,Lb,'Lbl',True)]
     circuit += [J(3,2,10),J(4,2,10)]
-    circuit += [C(3,2,30),C(4,2,30)]
-    circuit += [C(2,5,30),C(2,6,30)]
+    circuit += [C(3,2,Cb),C(4,2,Cc)]
+    circuit += [C(2,5,Cd),C(2,6,Ce)]
     circuit += [J(2,5,10),J(2,6,100)]
-    circuit += [C(2,0,40)]
-    circuit += [L(5,7,.00002,'Ltr',True),L(6,7,.00035,'Lbr',True)]
-    circuit += [L(1,7,.0005,'Ll',True)]
-    circuit += [C(7,0,10)]
+    circuit += [C(2,0,Cf)]
+    circuit += [L(5,7,Lc,'Ltr',True),L(6,7,Ld,'Lbr',True)]
+    circuit += [L(1,7,Le,'Ll',True)]
+    circuit += [C(7,0,Cg)]
     circuit = Circuit(circuit,basis)
     return circuit
 
