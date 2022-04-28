927005c3f43a90052e404113aa219efb07b7f1fa
shahrukh <shahrukh.chishti@gmail.com>
2022-04-28 08:14:38 +0530

    AnaGS : basic implementation

--- AnalyticGS/ground_state_search.py
+++ AnalyticGS/ground_state_search.py
@@ -1,7 +1,13 @@
 import torch,numpy
-from torch.autograd import Variable
+from torch import tensor
 from scipy.optimize import minimize as sp_minimize
 
+def squarePolynomial(poly):
+    N = len(poly)-1
+    poly = numpy.array([poly])
+    square = poly.T@poly
+    square = numpy.fliplr(square)
+    return [square.trace(offset=-k) for k in numpy.arange(-N,N+1,1)]
 
 def polynomial(poly):
     N = len(poly)-1
@@ -36,18 +42,20 @@ def curvature(poly):
         return 2*func*f(x) + 2*(I(x)**2)
     return IInd
 
-def next(x,I,II):
-    update = I(x)/II(x)
+def next(x,f,I,II):
+    update = f(x)*I(x)/(I(x)**2-f(x)*II(x))/2
     return x-update
 
 def solve_smallest_root(poly,x,iterations):
-    f = polynomial(poly)
-    I = gradient(poly)
-    II = curvature(poly)
+    f = numpy.polynomial.Polynomial(poly)
+    I = f.deriv(1)
+    II = f.deriv(2)
+    X = []
     for iteration in range(iterations):
-        print(x,I(x),II(x))
-        x = next(x,f,I)
-    return x
+        #print(x,I(x),II(x))
+        x = next(x,f,I,II)
+        X.append(x)
+    return x,X
 
 def scipy_ground_state(poly,iteration_set,starting_set):
     f = polynomial(poly)
@@ -61,3 +69,6 @@ def scipy_ground_state(poly,iteration_set,starting_set):
             ground_solve[i,j] = res.x[0]
     return ground_solve
 
+if __name__ == '__main__':
+    quadratic = [1,2,23,1,2,-1]
+    print(squarePolynomial(quadratic))
