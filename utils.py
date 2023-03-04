2206fb172877fac73b68f6b7d795e8374df16e00
Shahrukh Chishti <chishti@pgi8cluster15.iff.kfa-juelich.de>
2023-03-04 09:32:58 +0000

    plotly render

--- utils.py
+++ utils.py
@@ -6,33 +6,77 @@ from IPython.display import Image, display
 from pyvis import network as pvnet
 import networkx as nx
 from networkx.drawing.nx_pydot import write_dot
-from numpy import full,nan,zeros
+from numpy import full,nan,zeros,arange
 
 def empty(shape):
     E = zeros(shape)
     E.fill(nan)
     return E
 
-def plotCombine(plot,title=None,x_label=None,y_label=None,mode='lines'):
+# analysis plots
+    
+def render(fig,title,size,html,export):
+    fig.update_layout(font={'size':14})
+    fig.update_layout(margin_t=30)
+    if size:
+        height,width = size
+        fig.update_layout(width=width,height=height)
+    if export:        
+        fig.write_image('./'+title+'.'+export)
+    fig.update_layout(title=title)
+    fig.update_layout(margin_t=100)
+    if html:    
+        fig.write_html('./'+title+'.html')
+    else :
+        py.iplot(fig)
+
+def plotCombine(plot,title=None,x_label=None,y_label=None,mode='lines',size=None,html=False,export=None):
     fig = go.Figure()
     for name,(x,y) in plot.items():
         fig.add_trace(go.Scatter(x=x,y=y,name=name,mode=mode))
-    fig.update_layout(title=title,
-    xaxis_title=x_label,
-    yaxis_title=y_label)
-    py.iplot(fig)
+    fig.update_layout(xaxis_title=x_label,yaxis_title=y_label)
+    
+    render(fig,title,size,html,export)
 
-def plotCompare(x,plot,title=None,x_label=None,y_label=None,save=False):
+def plotCompare(x,plot,title=None,x_label=None,y_label=None,size=None,html=False,export=None,legend=True):
     fig = go.Figure()
     for name,values in plot.items():
         fig.add_trace(go.Scatter(x=x,y=values,name=name))
-    fig.update_layout(title=title,
-    xaxis_title=x_label,
-    yaxis_title=y_label)
-    if save:
-        fig.write_html('./'+title+'.html')
-    py.iplot(fig)
+    fig.update_layout(xaxis_title=x_label,yaxis_title=y_label)
+    
+    render(fig,title,size,html,export)
 
+def plotHeatmap(z,x,y,title=None,xaxis=None,yaxis=None,size=None,html=False,export=None):
+    fig = go.Figure()
+    heatmap = go.Heatmap(z=z,y=y,x=x)
+    fig.add_trace(heatmap)
+    fig.update_layout(xaxis_title=xaxis,yaxis_title=yaxis)
+    
+    render(fig,title,size,html,export)
+    
+def plotTrajectory(evo,plot,title=None,x_label=None,y_label=None,size=None,html=False,export=None):
+    fig = go.Figure()
+    evo = evo/max(evo)*10
+    for name,path in plot.items():
+        fig.add_trace(go.Scatter(x=path[:,0],y=path[:,1],name=name,mode='lines+markers',
+                                marker={'size':evo}))
+    fig.update_layout(xaxis_title=x_label,yaxis_title=y_label)
+    
+    render(fig,title,size,html,export)
+    
+def plotOptimization(z,x,y,paths,title=None,xaxis=None,yaxis=None,size=None,html=False,export=None):
+    fig = go.Figure()
+    heatmap = go.Heatmap(z=z,y=y,x=x,name='loss')
+    fig.add_trace(heatmap)
+    fig.update_layout(xaxis_title=xaxis,yaxis_title=yaxis)
+    for name,path in paths.items():
+        evo = arange(len(path)) ; evo = evo/max(evo)*10
+        fig.add_trace(go.Scatter(x=path[:,0],y=path[:,1],name=name,mode='lines+markers',marker={'size':evo}))
+        
+    render(fig,title,size,html,export)
+    
+# graph plots
+    
 def view_pydot(G):
     #import ipdb; ipdb.set_trace()
     pdot = to_pydot(G)
@@ -54,39 +98,3 @@ def plotDOTGraph(G,filename='temp'):
     #DOT = nx.nx_agraph.write_dot(G,filename+'.png')
     pdot.write_png(filename+'.png')
     #view_pydot(pdot)
-
-def plotHeatmap(z,x,y,title=None,xaxis=None,yaxis=None,aspect=None):
-    fig = go.Figure()
-    heatmap = go.Heatmap(z=z,y=y,x=x)
-    fig.add_trace(heatmap)
-    fig.update_layout(title=title,xaxis_title=xaxis,yaxis_title=yaxis)
-    if aspect:
-        fig.update_layout(width=aspect,height=aspect)
-        fig.update_layout(yaxis={'scaleanchor':'x','scaleratio':aspect})
-    py.iplot(fig)
-    
-def plotTrajectory(evo,plot,title=None,x_label=None,y_label=None,save=False):
-    fig = go.Figure()
-    evo = evo/max(evo)*10
-    for name,path in plot.items():
-        fig.add_trace(go.Scatter(x=path[:,0],y=path[:,1],name=name,mode='lines+markers',
-                                marker={'size':evo}))
-    fig.update_layout(title=title,
-    xaxis_title=x_label,
-    yaxis_title=y_label)
-    #fig.update_layout(hovermode='x unified')
-    if save:
-        fig.write_html('./'+title+'.html')
-    py.iplot(fig)
-    
-def plotOptimization(z,x,y,paths,title=None,xaxis=None,yaxis=None,aspect=None):
-    fig = go.Figure()
-    heatmap = go.Heatmap(z=z,y=y,x=x)
-    fig.add_trace(heatmap)
-    fig.update_layout(title=title,xaxis_title=xaxis,yaxis_title=yaxis)
-    for name,path in paths.items():
-        fig.add_trace(go.Scatter(x=path[:,0],y=path[:,1],name=name,mode='lines+markers'))
-    if aspect:
-        fig.update_layout(width=aspect,height=aspect)
-        fig.update_layout(yaxis={'scaleanchor':'x','scaleratio':aspect})
-    py.iplot(fig)
