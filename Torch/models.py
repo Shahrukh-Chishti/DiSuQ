d2eb2f736d7cfc443e7f97552c11586ed3ce51b6
Shahrukh Chishti <shahrukh.chishti@gmail.com>
2024-09-24 17:17:08 +0200

    default SPARSE=False

--- Torch/models.py
+++ Torch/models.py
@@ -1,8 +1,7 @@
-import numpy,scqubits
-from DiSuQ.Torch.circuit import Circuit, phase
 from DiSuQ.Torch.components import J,C,L
 from DiSuQ.Torch.components import C0,J0,L0,capE,indE,C_,J_,L_
-from torch import tensor,sqrt
+from torch import tensor
+SPARSE = False # default
 
 def tensorize(values,variable=True):
     tensors = []
@@ -15,7 +14,7 @@ def sigInv(sig,limit):
 
 # input order enumerates into node index
 
-def box4Branches(Rep,basis,Ej,Ec,El,sparse=True):
+def box4Branches(Rep,basis,Ej,Ec,El,sparse=SPARSE):
     circuit = [L(0,1,El[0],'L0',True,L0,L_),C(0,1,Ec[0],'C0',C0,C_),J(0,1,Ej[0],'J0',J0,J_)]
     circuit += [L(1,2,El[1],'L1',True,L0,L_),C(1,2,Ec[1],'C1',C0,C_),J(1,2,Ej[1],'J1',J0,J_)]
     circuit += [L(2,3,El[2],'L2',True,L0,L_),C(2,3,Ec[2],'C2',C0,C_),J(2,3,Ej[2],'J2',J0,J_)]
@@ -25,7 +24,7 @@ def box4Branches(Rep,basis,Ej,Ec,El,sparse=True):
     circuit = Rep(circuit,control_iD,basis,sparse=False)
     return circuit
 
-def zeroPi(Rep,basis,Ej=10.,Ec=50.,El=10.,EcJ=100.,sparse=True,symmetry=False,_L_=(L_,L0),_C_=(C_,C0),_J_=(J_,J0),_CJ_=(4*C_,4*C0),device=None):
+def zeroPi(Rep,basis,Ej=10.,Ec=50.,El=10.,EcJ=100.,sparse=SPARSE,symmetry=False,_L_=(L_,L0),_C_=(C_,C0),_J_=(J_,J0),_CJ_=(4*C_,4*C0),device=None):
     circuit = [L(0,1,El,'Lx',True,_L_[1],_L_[0]),L(2,3,El,'Ly',True,_L_[1],_L_[0])]
     circuit += [C(1,2,Ec,'Cx',_C_[1],_C_[0]),C(3,0,Ec,'Cy',_C_[1],_C_[0])]
     circuit += [J(1,3,Ej,'Jx',_J_[1],_J_[0]),J(2,0,Ej,'Jy',_J_[1],_J_[0])]
@@ -41,7 +40,7 @@ def zeroPi(Rep,basis,Ej=10.,Ec=50.,El=10.,EcJ=100.,sparse=True,symmetry=False,_L
     circuit = Rep(circuit,control_iD,basis,sparse,pairs,device)
     return circuit
 
-def prismon(basis,Ej=10.,Ec=50.,El=10.,EcJ=100.,sparse=True,symmetry=False,_L_=(L_,L0),_C_=(C_,C0),_J_=(J_,J0),_CJ_=(4*C_,4*C0)):
+def prismon(Rep,basis,Ej=10.,Ec=50.,El=10.,EcJ=100.,sparse=SPARSE,symmetry=False,_L_=(L_,L0),_C_=(C_,C0),_J_=(J_,J0),_CJ_=(4*C_,4*C0)):
     circuit =  [L(0,1,El,'La',True,_L_[1],_L_[0]),C(0,2,Ec,'Ca',_C_[1],_C_[0]),J(1,2,Ej,'Ja',_J_[1],_J_[0]),C(1,2,EcJ,'CJa',_CJ_[1],_CJ_[0])]
     circuit += [L(2,3,El,'Lb',True,_L_[1],_L_[0]),C(1,5,Ec,'Cb',_C_[1],_C_[0]),J(0,4,Ej,'Jb',_J_[1],_J_[0]),C(0,4,EcJ,'CJb',_CJ_[1],_CJ_[0])]
     circuit += [L(5,4,El,'Lc',True,_L_[1],_L_[0]),C(4,3,Ec,'Cc',_C_[1],_C_[0]),J(3,5,Ej,'Jc',_J_[1],_J_[0]),C(3,5,EcJ,'CJc',_CJ_[1],_CJ_[0])]
@@ -54,10 +53,11 @@ def prismon(basis,Ej=10.,Ec=50.,El=10.,EcJ=100.,sparse=True,symmetry=False,_L_=(
         pairs['Lb'] = 'La' ; pairs['Lc'] = 'La'
         pairs['CJb'] = 'CJa' ; pairs['CJc'] = 'CJa'
         
-    circuit = Circuit(circuit,basis,sparse,pairs)
+    control_iD = ('La','Lb','Lc')
+    circuit = Rep(circuit,control_iD,basis,sparse,pairs)
     return circuit
 
-def transmon(Rep,basis,Ej=10.,Ec=0.3,sparse=True):
+def transmon(Rep,basis,Ej=10.,Ec=0.3,sparse=SPARSE):
     transmon = [J(0,1,Ej,'J')]
     transmon += [C(0,1,Ec,'C')]
     control_iD = ()
@@ -65,19 +65,20 @@ def transmon(Rep,basis,Ej=10.,Ec=0.3,sparse=True):
     transmon = Rep(transmon,control_iD,basis,sparse)
     return transmon
 
-def splitTransmon(basis):
+def splitTransmon(Rep,basis,sparse=SPARSE):
     transmon = [J(0,1,10),C(0,1,100)]
     transmon += [L(1,2,.0003,'I',True)]
     transmon += [J(2,0,10),C(2,0,100)]
-    transmon = Circuit(transmon,basis)
+    control_iD = ('I')
+    transmon = Rep(transmon,control_iD,basis,sparse)
     return transmon
 
-def oscillatorLC(Rep,basis,El=.00031,Ec=51.6256,sparse=True):
-    oscillator = [L(0,1,El,'L'),C(0,1,Ec,'C')]
+def oscillatorLC(Rep,basis,El=.00031,Ec=51.6256,sparse=SPARSE):
+    circuit = [L(0,1,El,'L'),C(0,1,Ec,'C')]
     control_iD = ()
     return Rep(circuit,control_iD,basis,sparse)
 
-def fluxoniumArray(basis,shunt=None,gamma=1.5,N=0,Ec=100,Ej=150,sparse=True):
+def fluxoniumArray(Rep,basis,shunt=None,gamma=1.5,N=0,Ec=100,Ej=150,sparse=SPARSE):
     # N : number of islands
     circuit = [C(0,1,Ec,'Cap')]
     circuit += [J(0,1,Ej,'Junc')]
@@ -89,10 +90,11 @@ def fluxoniumArray(basis,shunt=None,gamma=1.5,N=0,Ec=100,Ej=150,sparse=True):
     circuit += [J(1+N,0,gamma*Ej,'junc'+str(N))]
     circuit += [C(1+N,0,shunt,'cap'+str(N),C_=0.)]
     
-    circuit = Circuit(circuit,basis,sparse)
+    control_iD = ()
+    circuit = Rep(circuit,control_iD,basis,sparse)
     return circuit
 
-def fluxonium(Rep,basis,El=.0003,Ec=.1,Ej=20,sparse=True):
+def fluxonium(Rep,basis,El=.0003,Ec=.1,Ej=20,sparse=SPARSE):
     circuit = [C(0,1,Ec,'Cap')]
     circuit += [J(0,1,Ej,'JJ')]
     circuit += [L(0,1,El,'I',True)]
@@ -101,7 +103,7 @@ def fluxonium(Rep,basis,El=.0003,Ec=.1,Ej=20,sparse=True):
     circuit = Rep(circuit,control_iD,basis,sparse)
     return circuit
 
-def shuntedQubit(Rep,basis,josephson=[120.,50,120.],cap=[10.,50.,10.],ind=100.,sparse=True,symmetry=False,_C_=(C_,C0),_J_=(J_,J0)):
+def shuntedQubit(Rep,basis,josephson=[120.,50,120.],cap=[10.,50.,10.],ind=100.,sparse=SPARSE,symmetry=False,_C_=(C_,C0),_J_=(J_,J0)):
     Ej1,Ej2,Ej3 = josephson
     C1,C2,C3 = cap
 
@@ -119,7 +121,7 @@ def shuntedQubit(Rep,basis,josephson=[120.,50,120.],cap=[10.,50.,10.],ind=100.,s
     circuit = Rep(circuit,['I'],basis,sparse,pairs)
     return circuit
 
-def phaseSlip(basis,inductance=[.001,.0005,.00002,.00035,.0005],capacitance=[100,30,30,30,30,40,10]):
+def phaseSlip(Rep,basis,sparse=SPARSE,inductance=[.001,.0005,.00002,.00035,.0005],capacitance=[100,30,30,30,30,40,10]):
     La,Lb,Lc,Ld,Le = inductance
     Ca,Cb,Cc,Cd,Ce,Cf,Cg = capacitance
     circuit = [C(0,1,Ca)]
@@ -132,14 +134,16 @@ def phaseSlip(basis,inductance=[.001,.0005,.00002,.00035,.0005],capacitance=[100
     circuit += [L(5,7,Lc,'Ltr',True),L(6,7,Ld,'Lbr',True)]
     circuit += [L(1,7,Le,'Ll',True)]
     circuit += [C(7,0,Cg)]
-    circuit = Circuit(circuit,basis)
+    control_iD = ('Ltl','Lbl','Ltr','Lbr','Ll')
+    circuit = Rep(circuit,control_iD,basis,sparse)
     return circuit
 
 if __name__=='__main__':
-    circuit = shuntedQubit()
+    from DiSuQ.Torch.circuit import Kerman
+    basis = {'O':[10],'I':[0],'J':[10,10]}
+    circuit = shuntedQubit(Kerman,basis)
     print(circuit.modeDistribution())
-    circuit.basis = {'O':[10],'I':[0],'J':[10,10]}
-    H_LC = circuit.kermanHamiltonianLC()
-    H_J = circuit.kermanHamiltonianJosephson({'I':tensor(.25)})
+    H_LC = circuit.hamiltonianLC()
+    H_J = circuit.hamiltonianJosephson({'I':tensor(.25)})
 
     import ipdb; ipdb.set_trace()
